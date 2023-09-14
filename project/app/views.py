bf347f6ce83d19a654fc2cdbeff1d569d73d8db4
adibaba <>
2023-09-14 11:22:10 +0200

    multiple paths

--- project/app/views.py
+++ project/app/views.py
@@ -1,5 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
 
 def index(request):
     return HttpResponse('hello world')
+
+
+def echo(request, string):
+    if string == 'hello':
+        return HttpResponseRedirect(reverse('echo', args=['world']))
+    else:
+        return HttpResponse(f'<b>{string}</b>')
