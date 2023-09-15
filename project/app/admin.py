00e2b1b012aca726a1b1a7bdac182b56623d6773
adibaba <>
2023-09-15 11:56:54 +0200

    simple admin

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Bookmark
 
-# Register your models here.
+
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('url', 'title', 'id')
+
+
+admin.site.register(Bookmark, BookmarkAdmin)
