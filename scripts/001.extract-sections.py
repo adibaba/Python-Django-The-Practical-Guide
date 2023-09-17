5ac316061c4da53fadc5214039c440b355cec253
adibaba <>
2023-09-17 21:32:44 +0200

    removed scripts

--- scripts/001.extract-sections.py
+++
@@ -1,4 +0,0 @@
-# Extracts section names from copy-pasted text from Udemy navigatoin HTML
-for line in open('text.txt', 'r').readlines():
-    if len(line.strip()) > 5:
-        print (line.strip())
