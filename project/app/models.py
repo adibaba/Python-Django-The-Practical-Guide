8d75e687385ad4bd228f73c05b5c0c6eee0c0221
adibaba <>
2023-09-18 12:50:38 +0200

    related_name

--- project/app/models.py
+++ project/app/models.py
@@ -8,7 +8,8 @@ class Author(models.Model):
 class Bookmark(models.Model):
     url = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name='bookmarks')
 
     def __str__(self):
         return f"{self.url} ({self.title})"
