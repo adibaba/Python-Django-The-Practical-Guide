1d773cf2da96ed06a31c6822eb9c240a63ce8413
adibaba <>
2023-09-18 14:29:21 +0200

    m:n

--- project/app/models.py
+++ project/app/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=80)
+
+    def __str__(self):
+        return self.name
+
+
 class Address(models.Model):
     city = models.CharField(max_length=255)
 
@@ -25,6 +32,7 @@ class Bookmark(models.Model):
     title = models.CharField(max_length=255)
     author = models.ForeignKey(
         Author, on_delete=models.CASCADE, null=True, related_name='bookmarks')
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return f"{self.url} ({self.title})"
