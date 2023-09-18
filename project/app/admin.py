41f87a9749a5a36ca4827f2ce3381b2b6b2741cc
adibaba <>
2023-09-18 13:43:27 +0200

    added author to admin

--- project/app/admin.py
+++ project/app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Bookmark
+from .models import Bookmark, Author
 
 
 class BookmarkAdmin(admin.ModelAdmin):
@@ -7,3 +7,4 @@ class BookmarkAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Bookmark, BookmarkAdmin)
+admin.site.register(Author)
--- project/app/models.py
+++ project/app/models.py
@@ -4,6 +4,9 @@ from django.db import models
 class Author(models.Model):
     name = models.CharField(max_length=255)
 
+    def __str__(self):
+        return f"{self.name}"
+
 
 class Bookmark(models.Model):
     url = models.CharField(max_length=255)
