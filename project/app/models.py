83a8a842fb52f44e433f93f9b5a0de0438aa7758
adibaba <>
2023-09-18 12:18:26 +0200

    1:1 relation

--- project/app/models.py
+++ project/app/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=255)
+
+
 class Bookmark(models.Model):
     url = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f"{self.url} ({self.title})"
