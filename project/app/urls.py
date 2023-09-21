4a97c7ce9911a5f5fde6c80db1d3e9f37c2cbe3b
adibaba <>
2023-09-21 09:39:42 +0200

    form template

--- project/app/urls.py
+++ project/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('details/<int:bookmark_id>', views.details, name='details'),
+    path('form/', views.form, name='form'),
 ]
--- project/app/views.py
+++ project/app/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Bookmark
+from django.utils.safestring import mark_safe
 
 
 def index(request):
     return render(request, "app/index.html", {
         'bookmarks': Bookmark.objects.all(),
+        'content': mark_safe('<a href="form/">Form</a>'),
     })
 
 
@@ -14,4 +16,9 @@ def details(request, bookmark_id):
         'bookmark_id': bookmark.id,
         'bookmark_title': bookmark.title,
         'bookmark_url': bookmark.url,
+    })
+
+
+def form(request):
+    return render(request, "app/form.html", {
     })
